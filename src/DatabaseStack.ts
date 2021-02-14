a8a4d834868b1b6f730fcb5a78e71139b9ad4225
Matthew Bonig <matthew.bonig@gmail.com>
2021-02-13 19:19:31 -0700

    adding databaseuser

--- src/DatabaseStack.ts
+++ src/DatabaseStack.ts
@@ -1,6 +1,7 @@
 import { Vpc } from '@aws-cdk/aws-ec2';
 import { DatabaseInstance, DatabaseInstanceEngine, MysqlEngineVersion } from '@aws-cdk/aws-rds';
 import { Construct, Stack, StackProps } from '@aws-cdk/core';
+import { DatabaseUser } from '@matthewbonig/rds-tools';
 
 interface DatabaseStackProps extends StackProps {
   /**
@@ -11,12 +12,19 @@ interface DatabaseStackProps extends StackProps {
 
 export class DatabaseStack extends Stack {
   public databaseInstance: DatabaseInstance;
+
   constructor(scope: Construct, id: string, props: DatabaseStackProps) {
     super(scope, id, props);
 
     this.databaseInstance = new DatabaseInstance(this, 'db', {
       engine: DatabaseInstanceEngine.mysql({ version: MysqlEngineVersion.VER_8_0_21 }),
       vpc: props.vpc,
+    });
+
+    new DatabaseUser(this, 'service-account', {
+      databaseInstance: this.databaseInstance,
+      username: 'app',
+      databases: ['somedatabase'],
     });
   }
 }
